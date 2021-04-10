058b20e180e9173a0a2c1ce1e3a8e4f9c6938f8f
doxas <eizanco@gmail.com>
2021-04-10 13:28:04 +0900

    implement uuid generator

--- src/script.js
+++ src/script.js
@@ -2129,4 +2129,20 @@ function copyToClipboard(str){
     document.body.removeChild(t);
 }
 
+function uuid(){
+    // https://github.com/GoogleChrome/chrome-platform-analytics/blob/master/src/internal/identifier.js
+    const chars = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.split('');
+    for(let i = 0, j = chars.length; i < j; i++){
+        switch(chars[i]){
+            case 'x':
+                chars[i] = Math.floor(Math.random() * 16).toString(16);
+                break;
+            case 'y':
+                chars[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+                break;
+        }
+    }
+    return chars.join('');
+}
+
 })();
